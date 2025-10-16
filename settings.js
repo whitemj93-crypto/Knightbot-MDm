82bbec598dc9d777d574777187cbe91aed80906e
whitemj93-crypto <whitemj93@gmail.com>
2025-10-16 21:37:25 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'MJ_WHITE', // Your name
+  ownerNumber: '27659952861', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
@@ -11,6 +11,6 @@ const settings = {
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.2.2",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
-};
+
 
 module.exports = settings;
